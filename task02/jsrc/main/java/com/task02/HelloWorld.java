16151a696ea7d518cadcc68882a714776c96ad65
yathishg01 <yathish_g@epam.com>
2024-07-25 21:58:42 +0530

    Task02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -9,33 +9,49 @@ import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        roleName = "hello_world-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
 )
 public class LambdaFunctionHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-	@Override
-	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
-		context.getLogger().log("Received event: " + event);
+    @Override
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent event, Context context) {
+        APIGatewayV2HTTPResponse response;
 
-		// Check if the request is for the /hello endpoint with the GET method
-		if ("/hello".equals(event.getRawPath()) && "GET".equalsIgnoreCase(event.getRequestContext().getHttp().getMethod())) {
-			return APIGatewayV2HTTPResponse.builder()
-					.withStatusCode(200)
-					.withHeaders(Collections.singletonMap("Content-Type", "application/json"))
-					.withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}")
-					.build();
-		} else {
-			String path = event.getRawPath();
-			String method = event.getRequestContext().getHttp().getMethod();
-			String errorMessage = String.format("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: %s. HTTP method: %s\"}", path, method);
+        try {
+            String path = event.getRawPath();
+            String method = event.getRequestContext() != null
+                    ? event.getRequestContext().getHttp() != null
+                    ? event.getRequestContext().getHttp().getMethod()
+                    : null
+                    : null;
 
-			return APIGatewayV2HTTPResponse.builder()
-					.withStatusCode(400)
-					.withHeaders(Collections.singletonMap("Content-Type", "application/json"))
-					.withBody(errorMessage)
-					.build();
-		}
-	}
+            if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
+                return APIGatewayV2HTTPResponse.builder()
+                        .withStatusCode(200)
+                        .withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}")
+                        .build();
+            } else {
+                return APIGatewayV2HTTPResponse.builder()
+                        .withStatusCode(400)
+                        .withBody("{\"statusCode\": 400, \"message\": \"Bad request syntax or unsupported method. Request path: " + (path != null ? path : "null") + ". HTTP method: " + (method != null ? method : "null") + "\"}")
+                        .build();
+            }
+        } catch (Exception e) {
+            // Log exception and return an error response
+            context.getLogger().log("Exception: " + e.getMessage());
+            response = APIGatewayV2HTTPResponse.builder()
+                    .withStatusCode(500)
+                    .withBody("{\"statusCode\": 500, \"message\": \"Internal Server Error\"}")
+                    .build();
+        }
+
+        return response;
+    }
+
 }
