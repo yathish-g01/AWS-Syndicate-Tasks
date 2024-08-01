d26f7128d682bbc77a4569b9d9fa3ead93cbcfa0
yathishg01 <yathish_g@epam.com>
2024-08-02 00:13:22 +0530

    configured api gateway meta

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -1,14 +1,17 @@
 package com.task05;
 
-import com.amazonaws.regions.Regions;
+
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
 import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
@@ -18,9 +21,7 @@ import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
-import com.task05.dto.Event;
-import com.task05.dto.Request;
-import com.task05.dto.Response;
+import java.util.LinkedHashMap;
 
 import java.time.LocalDateTime;
 import java.util.Map;
@@ -40,62 +41,68 @@ import java.util.UUID;
 		@EnvironmentVariable(key = "region", value = "${region}"),
 		@EnvironmentVariable(key = "table", value = "${target_table}")})
 @DependsOn(name = "Events", resourceType = ResourceType.DYNAMODB_TABLE)
-public class ApiHandler implements RequestHandler<Request, Response> {
+public class ApiHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-	private Gson gson;
-	private ObjectContentMapper mapper;
-	private DynamoDBMapper dynamoDB;
-	private LambdaLogger log;
-
-	private final int STATUS_OK = 201;
-
-	private void init(Context context){
-		log = context.getLogger();
-		gson = new GsonBuilder().create();
-		mapper = new ObjectContentMapper(gson);
-		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.EU_CENTRAL_1).build();
-		dynamoDB = new DynamoDBMapper(client);
-	}
-
+	private static final int STATUS_CREATED = 201;
+	private static final String TABLE_NAME = "cmtr-6c6b70bd-Events";
+	private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
 
 	@Override
-	public Response handleRequest(Request request, Context context) {
-		init(context);
-		log.log(gson.toJson(request));
-		return new Response(STATUS_OK, addDataAndResponse(request));
-	}
-
-	private Event addDataAndResponse(Request request){
-		String id = UUID.randomUUID().toString();
-		LocalDateTime createdAt = LocalDateTime.now();
-		Event event = new Event();
-		event.setId(id);
-		event.setCreatedAt(createdAt.toString());
-		event.setPrincipalId(request.getPrincipalId());
+	public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent request, Context context) {
 		try {
-			event.setBody(mapper.objectToContent(request.getContent()));
-		} catch (Exception e) {
-			log.log("WARNING: Failed to convert request body to event content");
-			event.setBody(
-					Map.of("error",
-							"wrong content provided, expected \"content\" {\"String\" : \"String\" ...}" +
-									"but was" + gson.toJson(request.getContent())
-					)
-			);
+			System.out.println("Table name: " + TABLE_NAME);
+			return handlePost(request);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
 		}
-		log.log("Event created: " + gson.toJson(event));
-		return saveEvent(event);
 	}
 
-	private Event saveEvent(Event event){
-		DynamoDBEvent item = new DynamoDBEvent();
-		item.setId(event.getId());
-		item.setPrincipalId(event.getPrincipalId());
-		item.setCreatedAt(event.getCreatedAt());
-		item.setBody(event.getBody());
-		dynamoDB.save(item);
-		log.log("Item saved as: " + gson.toJson(item));
-		return event;
+	private APIGatewayV2HTTPResponse handlePost(APIGatewayV2HTTPEvent request) throws JsonProcessingException {
+		Map<String, Object> result = saveToDynamoDb(request);
+		APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+		response.setStatusCode(STATUS_CREATED);
+		response.setHeaders(responseHeaders);
+		response.setBody(new ObjectMapper().writeValueAsString(result));
+		return response;
 	}
 
+	private Map<String, Object> saveToDynamoDb(APIGatewayV2HTTPEvent requestEvent) throws JsonProcessingException {
+		final AmazonDynamoDB amazonDynamoDB = AmazonDynamoDBClientBuilder.standard().build();
+		ObjectMapper objectMapper = new ObjectMapper();
+		System.out.println("Request body: " + requestEvent.getBody());
+		Map<String, Object> inputJson = objectMapper.readValue(requestEvent.getBody(), new TypeReference<Map<String, Object>>() {});
+
+		String id = UUID.randomUUID().toString();
+		String principalId = inputJson.get("principalId").toString();
+		System.out.println("principalId: " + principalId);
+		String createdAt = getISOFormattedDateTime();
+		Object body = inputJson.get("content");
+		if (body == null) {
+			throw new IllegalArgumentException("argument \"content\" is null");
+		}
+		System.out.println("content: " + body);
+
+		Map<String, AttributeValue> itemValues = new LinkedHashMap<>();
+		itemValues.put("id", new AttributeValue().withS(id));
+		itemValues.put("principalId", new AttributeValue().withS(principalId));
+		itemValues.put("createdAt", new AttributeValue().withS(createdAt));
+		itemValues.put("body", new AttributeValue().withS(objectMapper.writeValueAsString(body))); // Serialize the body object to JSON string
+
+		System.out.println(itemValues);
+
+		PutItemRequest putItemRequest = new PutItemRequest().withTableName(TABLE_NAME).withItem(itemValues);
+		amazonDynamoDB.putItem(putItemRequest);
+
+		// Return the result as a map
+		Map<String, Object> result = new LinkedHashMap<>();
+		result.put("id", id);
+		result.put("principalId", principalId);
+		result.put("createdAt", createdAt);
+		result.put("body", body);
+		return result;
+	}
+
+	private String getISOFormattedDateTime() {
+		return java.time.Instant.now().toString();
+	}
 }
